15a5af64a9a741ee2adcdae3d29f6f168d39b0a5
Sam M. Ward <smward1@hotmail.com>
2023-06-12 15:38:07 +0100

    analysis scripts up to converting corrected snpy files to snana

--- analysis/analysis1_load_data.py
+++ analysis/analysis1_load_data.py
@@ -1,1 +1,13 @@
-from analysis0 import *
+#Append model_files to path directory
+import sys
+sys.path.append('../model_files/')
+from model1_load_data import *
+#Load up and save raw data as snpyfile dictionaries, load up metadata
+dataloader = LOAD_DATA()
+
+from birdsnack_model import BIRDSNACK
+
+for path_file_survey in dataloader.choices['load_data_parameters']['load_path_file_survey']:
+    print ('###'*3)
+    if path_file_survey[2]=='Misc':
+        bs = BIRDSNACK(loader={'path_file_survey':path_file_survey}, configname='loader_config.yaml', dfmeta=dataloader.dfmeta)
