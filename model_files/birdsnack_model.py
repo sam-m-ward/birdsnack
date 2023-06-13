15266508f423cb9dfb765068690efdea2917fd45
Sam M. Ward <smward1@hotmail.com>
2023-06-13 10:50:48 +0100

    Built model up to loading snana lcs with appropriate metadata e.g. boosted fluxerrs, trimmed filters, and stored mass and spectype data

--- model_files/birdsnack_model.py
+++ model_files/birdsnack_model.py
@@ -1,4 +1,21 @@
-import pickle,yaml
+"""
+BIRDSNACK Module
+
+Module contains BIRDSNACK class for default analysis: apply corrections, interpolate rest-frame data, analyse with HBM
+
+Contains
+--------------------
+BIRDSNACK class
+    inputs: configname='analysis_config.yaml',loader={},edit_dict = {},dfmeta=None,update_vars=False
+
+    Methods are:
+        X()
+		Y()
+
+--------------------
+"""
+
+import os,pickle,yaml
 from snoopy_corrections import SNOOPY_CORRECTIONS
 from miscellaneous import ensure_folders_to_file_exist
 
@@ -13,18 +30,28 @@ class BIRDSNACK:
 
 	Parameters
 	----------
-	configname: str
+	configname: str (optional; default='analysis_config.yaml')
 		name of config.yaml file used for analysis
 
-	edit_dict: dict
+	loader : dict (optional; default={})
+		dictionary of information important to analysis
+		e.g. loader['path_file_survey'] = [path,file,survey]
+
+	edit_dict: dict (optional; default={})
 		used to edit individual key,value pairs in the config.yaml choices file
+
+	dfmeta : pandas df (optional; default = None)
+		DataFrame of SN host galaxy stellar masses and spectroscopic subclassifications
+
+	update_vars : bool (optional; default=False)
+		if True, reload BIRDSNACK with updated .yaml choices
 
 	Methods
 	----------
 
 	"""
 
-	def __init__(self,loader={},configname='analysis_config.yaml',edit_dict = {},load_dictionary={},dfmeta=None,update_vars=True):
+	def __init__(self,configname='analysis_config.yaml',loader={},edit_dict = {},dfmeta=None,update_vars=False):
 		#Set Configname
 		self.configname   = configname
 
@@ -48,18 +75,42 @@ class BIRDSNACK:
 		#Load up metadata
 		self.dfmeta = dfmeta
 
-		#If loading up snpytxtfiles, load up the SNSsnpy dictionary
-		if self.choices['load_data_parameters']['load_mode']=='snpytxtfiles':
+		#If loading up snpytxtfiles or snanalcs, load up the SNSsnpy dictionary
+		if self.choices['load_data_parameters']['load_mode'] in ['snpytxtfiles','snanalcs']:
 			path,file,survey = loader['path_file_survey']
 			with open(self.SNSpath+file,'rb') as f:
 				self.SNSsnpy = pickle.load(f)
 
-		#Apply snpy corrections to SNSsnpy snpy files if load_mode=='snpytxtfiles' to create corrected snana lcs dictionary
-		if self.choices['load_data_parameters']['apply_snpy_corrections']:
-			if self.choices['load_data_parameters']['load_mode']!='snpytxtfiles':
-				raise Exception("If applying SNooPy corrections via apply_snpy_corrections=True, make sure load_mode is set to 'snpytxtfiles' ")
-			else:
+			#Apply snpy corrections to SNSsnpy snpy files if load_mode=='snpytxtfiles' to create corrected snana lcs dictionary
+			if self.choices['load_data_parameters']['load_mode'] == 'snpytxtfiles':
 				snpycorr = SNOOPY_CORRECTIONS(self.choices, self.snanapath)
 				for sn in self.SNSsnpy:
 					snpycorr.correct_sn(sn, self.SNSsnpy[sn])
-					print (snpycorr.lc)
+
+			#Load up snana lcs and apply SNR cut, error_boost_dict, and trim to interpflts
+			self.load_and_preprocess_snana_lcs()
+
+	def load_and_preprocess_snana_lcs(self):
+		from snana_functions import get_snana_foldername, correct_lc_data_SNR_boostdict, get_mag, trim_filters, set_lcmeta_ordered_flts, update_lcmetadata
+		import snanaio as io
+		self.lcs = {}
+		self.snana_folder = self.snanapath+get_snana_foldername(self.choices['snpy_parameters'])
+		for sn in self.SNSsnpy:
+			path_snana_product  = f"{self.snana_folder}{sn}_{self.SNSsnpy[sn]['survey']}.snana.dat"
+			if not os.path.exists(path_snana_product):
+				print ('###'*3)
+				print (f'snana lc not created yet for SN{sn}, thus load in snsnpy, apply corrections, and create snana lc')
+				snpycorr = SNOOPY_CORRECTIONS(self.choices, self.snanapath)
+				snpycorr.correct_sn(sn, self.SNSsnpy[sn])
+
+			#Load File
+			sn , lc  = io.read_snana_lcfile(path_snana_product)
+			#Apply SNR cut, boost flux errors, get magnitudes column
+			lc       = get_mag(correct_lc_data_SNR_boostdict(lc, self.choices['snpy_parameters']['snrcut'], self.choices['preproc_parameters']['error_boost_dict']))
+			#Trim to filters in interpflts
+			lc       = set_lcmeta_ordered_flts(trim_filters(lc, self.choices['snpy_parameters']['interpflts']))
+			#Update lcmeta with mass and spectral type
+			lc       = update_lcmetadata(lc,self.dfmeta[self.dfmeta['SN']==sn])
+
+			#Append lc to dictionary
+			self.lcs[sn] = lc
