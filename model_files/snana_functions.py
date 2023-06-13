3678d533e72fc6bd79c8e14e49fddccfc718eee5
Sam M. Ward <smward1@hotmail.com>
2023-06-13 15:43:45 +0100

    Added various lc functions for trimming sample

--- model_files/snana_functions.py
+++ model_files/snana_functions.py
@@ -185,6 +185,7 @@ def update_lcmetadata(lc,dfsn,snpy_product):
 	----------
 	lc with updated .meta
 	"""
+	#Insert Mass metadata
 	try:
 		lc.meta['Mbest'] = dfsn['Mbest'].mean()
 	except:
@@ -192,7 +193,7 @@ def update_lcmetadata(lc,dfsn,snpy_product):
 		if Ms==[]: 	lc.meta['Mbest'] = None
 		else:		lc.meta['Mbest'] = np.average(Ms)
 
-
+	#Insert Spectype metadata
 	spectypes = list(dfsn['SubtypeIa'].values)
 	if spectypes==[]:
 		lc.meta['Spectral_Type']=None
@@ -201,13 +202,174 @@ def update_lcmetadata(lc,dfsn,snpy_product):
 	else:
 		lc.meta['Spectral_Type']='_'.join(spectypes)
 
-	#Set GP restframe Tmax estimate to None, not yet estimated
+	#Initialise GP restframe Tmax estimate to None, not yet estimated
 	lc.meta['Tmax_GP_restframe']     = None
 	lc.meta['Tmax_GP_restframe_std'] = None
-	#Load up the SNooPy fitted Tmax estimate
+
+	#Insert SNooPy fitted Tmax estimate
 	try:
 		lc.meta['Tmax_snpy_fitted']  = snpy_product['Tmax_snpy_fitted']
 	except:
 		lc.meta['Tmax_snpy_fitted']  = None
 
 	return lc
+
+
+def get_lcf(lc,flt):
+	"""
+	Get Light Curve in filter f
+
+	Returns the light curve by applying a filter mask
+
+	Parameters
+	----------
+	lc: :py:class:`astropy.table.Table`
+		light curve object
+
+	flt: str
+		passband to keep
+
+	Returns
+	----------
+	lcf: :py:class:`astropy.table.Table`
+		light curve object in passband
+	"""
+	return lc[lc["flt"]==flt]
+
+def create_phase_column(lc,Tmax,phasemin=None,phasemax=None):
+	"""
+	Create Phase Column
+
+	Returns light curve object including new phase column, cut on phasemin<=phase<=phasemax
+
+	Parameters
+	----------
+	lc: :py:class:`astropy.table.Table`
+		light curve object
+
+	Tmax: float
+		Time of maximum brightness in measured in some reference frame band
+
+	phasemin: float or int (optional; default=None)
+		Apply cut on minimum phase
+
+	phasemax: float or int (optional; default=None)
+		Apply cut on maximum phase
+
+	Returns
+	----------
+	lc with phase column, cut so that phasemin<=phase<=phasemax
+	"""
+	if Tmax is not None:
+		lc['phase'] = (lc['mjd']-Tmax)/(1+lc.meta['REDSHIFT_HELIO'])
+	if phasemin is not None:
+		lc = lc[lc['phase']>=phasemin]
+	if phasemax is not None:
+		lc = lc[lc['phase']<=phasemax]
+	return lc
+
+
+def trim_lcf(lcf,Nbeforemax,Nbeforegap,Naftermax,Naftergap,tref=0,tcol='phase',local=False):
+    """
+    Trim Light Curve in Single Filter f
+
+    Returns True to trim or False to Retain
+
+    Parameters
+    ----------
+    lcf: :py:class:`astropy.table.Table`
+        light curve objects
+
+    Nbeforemax,Naftermax: floats or ints or lists
+        if float or int, the Number of data points before or after tref required to retain Ndata<Nmax is trim==True
+        if list, iterate over according to Ngaps (below)
+
+    Nbeforegap,Naftergap: floats or ints or lists
+        if float or int, is the time window (e.g. 5days) where data must reside in (before or after tref)
+        if list, iterate over (e.g. 1 point within 5 days, 3 points within 10 days)
+
+    tref: float or int (optional, default=0)
+        time or phase e.g. t=0 is Tmax
+
+    tcol: str (optional; default='phase')
+        either phase or mjd, choose column for delta t
+
+    local: bool (optional; default=False)
+        if False, then e.g. Nbeforegap=-10 is -10 days in absolute terms
+        if True, then e.g. Nbeforegap=-10 is 10 days before the reference phase, e.g. tref=15, so require data point in window 5-15 days
+
+    """
+    if local:
+        lcf[tcol] = copy.deepcopy(lcf[tcol]-tref)
+        tref      = 0
+    def Nwindow_before(lcf,L,U=tref,colname=tcol): return len(lcf[ (-L<lcf[colname]) & (lcf[colname]<U) ])
+    def Nwindow_after( lcf,U,L=tref,colname=tcol): return len(lcf[ ( L<lcf[colname]) & (lcf[colname]<U) ])
+
+    if type(Nbeforegap)==float or type(Nbeforegap)==int:
+        Ndata_beforemax = Nwindow_before(lcf,Nbeforegap)
+        if Ndata_beforemax<Nbeforemax:
+            #print ('Hey1')
+            return True
+    elif type(Nbeforegap)==list:
+        Ndata_beforemax_lens = [Nwindow_before(lcf,Nbg) for Nbg in Nbeforegap]
+        for i,Ndb in enumerate(Ndata_beforemax_lens):
+            if Ndb<Nbeforemax[i]:
+                #print ('Hey2', Ndb,Nbeforemax[i])
+                return True
+
+    if type(Naftergap)==float or type(Naftergap)==int:
+        Ndata_aftermax  = Nwindow_after(lcf,Naftergap)
+        if Ndata_aftermax<Naftermax: #e.g. if less than 1 data point before max within 5 days
+            #print ('Hey3')
+            return True
+
+    elif type(Nbeforegap)==list:
+        Ndata_aftermax_lens = [Nwindow_after(lcf,Nag) for Nag in Naftergap]
+        for i,Ndb in enumerate(Ndata_aftermax_lens):
+            if Ndb<Naftermax[i]:
+                #print ('Hey4',Ndb,Naftermax[i], Ndata_aftermax_lens)
+                return True
+    return False
+
+def get_time_lc_arrays(lcf,mjd_or_phase='mjd',flux_or_mag='flux'):
+	"""
+	Get time lc arrays
+
+	Simple function to get light curve object columns
+
+	Parameters
+	----------
+	lcf: :py:class:`astropy.table.Table`
+		light curve object
+
+	mjd_or_phase: 'mjd' or 'phase' or 'both' or 'neither' (optional; default='mjd')
+		string to indicate what time arrays to output
+
+	flux_or_mag: 'flux' or 'mag' or 'both' or 'neither' (optional; default='flux')
+		string to indicate what brightness (and brightness error) arrays to output
+
+	Returns
+	----------
+		some combination of mjd,phase,flux,fluxerr,mag,magerr (in that order)
+	"""
+	if mjd_or_phase=='mjd':
+		yield lcf["mjd"]
+	elif mjd_or_phase=='phase':
+		yield lcf["phase"]
+	elif mjd_or_phase=='both':
+		yield lcf["mjd"],lcf["phase"]
+	elif mjd_or_phase=='neither':
+		yield
+	else:
+		raise Exception ("Enter either 'mjd','phase','both','neither' for keyword mjd_or_phase")
+
+	if flux_or_mag=='flux':
+		yield from (lcf["fluxcal"],lcf["fluxcalerr"])
+	elif flux_or_mag=='mag':
+		yield from (lcf["mag"],lcf["magerr"])
+	elif flux_or_mag=='both':
+		yield from (lcf["fluxcal"],lcf["fluxcalerr"],lcf["mag"],lcf["magerr"])
+	elif flux_or_mag=='neither':
+		yield
+	else:
+		raise Exception ("Enter either 'flux','mag','both','neither' for keyword flux_or_mag")
