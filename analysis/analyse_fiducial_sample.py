960c8ae9d79ef0c1db763ce0f221c456b19cdbfe
Sam M. Ward <smward1@hotmail.com>
2023-06-15 15:23:47 +0100

    Added commenting to analyse_fiducial_sample.py

--- analysis/analyse_fiducial_sample.py
+++ analysis/analyse_fiducial_sample.py
@@ -2,16 +2,19 @@
 import sys
 sys.path.append('../model_files/')
 from load_raw_data import *
-
-#Load up fiducial sample
-dataloader = LOAD_DATA()
-SNSsnpy_fiducial = dataloader.load_SNSsnpy('SNSsnpy_fiducial.pkl')
-
 from birdsnack_model import BIRDSNACK
 
-bs = BIRDSNACK(loader={'SNSsnpy':SNSsnpy_fiducial}, configname='loader_config.yaml', dfmeta=dataloader.dfmeta)
+#Load up light curves of fiducial sample of SNe, and metadata
+dataloader = LOAD_DATA()
+SNSsnpy_fiducial = dataloader.load_SNSsnpy('SNSsnpy_fiducial.pkl')
+dfmeta           = dataloader.dfmeta
 
+#Load into Bird-Snack
+bs = BIRDSNACK(loader={'SNSsnpy':SNSsnpy_fiducial}, configname='loader_config.yaml', dfmeta=dfmeta)
+
+#Get peak magnitudes
 bs.get_peak_mags()
-#bs.plot_lcs()
+#Perform additional sample cuts
 bs.additional_cuts()
+#Fit HBM to data
 bs.fit_stan_model()
