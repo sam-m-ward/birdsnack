557ba05eee01bd5cad09310b56e7e997dcfc6360
Sam M. Ward <smward1@hotmail.com>
2023-06-15 12:42:37 +0100

    deleted analysis0 script

--- analysis/analysis0.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('../model_files/')
-
-#For analysis1
-from model1_load_data import *
-
-#Load up metadata including SN spectroscopic subclassification and host galaxy stellar masses
-#Also load up all lcs into snpy, and save survey name
-#dataloader = LOAD_DATA()
