b3e8bb17e7b77ae32c592de7c67abae9bb96caa0
Sam M. Ward <smward1@hotmail.com>
2023-06-27 13:22:47 +0100

    Added args options to save and or show plot

--- sbc/plot_sbc.py
+++ sbc/plot_sbc.py
@@ -23,6 +23,9 @@ parser.add_argument("--tauA",		default=0.5, 	      help='Float for non-loop AV D
 parser.add_argument("--loop_muRV",  default='1.5,2.5,3.5',help='Parameter values to loop over')
 parser.add_argument("--loop_sigRV", default='0.1,0.5,1.0',help='Parameter values to loop over')
 parser.add_argument("--loop_tauA",  default='0.2,0.5',    help='Parameter values to loop over')
+parser.add_argument("--save",	default=True,		      help='Save plot')
+parser.add_argument("--show",	default=False,		      help='Show plot')
+parser.add_argument("--quantilemode",	default=True,	  help='If True, annotate with 16,84, False, use sample std.')
 
 args = parser.parse_args().__dict__
 parnames      = ['muRV','sigRV','tauA']
@@ -51,20 +54,22 @@ if __name__ == "__main__":
 		#Get FITS
 		FITS = sbc.get_fits()
 		#Trim FITS to remove large Rhat
-		FITS = trim_to_KEEPERS({parvalue:FITS},get_KEEPERS({parvalue:FITS},Nsim_keep,Rhat_threshold,loop_par,dfpars[loop_par]))[parvalue]
+		FITS = trim_to_KEEPERS({'dummy':FITS},get_KEEPERS({'dummy':FITS},Nsim_keep,Rhat_threshold,loop_par,dfpars[loop_par]))['dummy']
 		#Store
 		GLOB_FITS[parvalue] = FITS
 
 	#Plot SBC
 	fig,axs = pl.subplots(len(GLOB_FITS),1,figsize=(8,6*len(GLOB_FITS)),sharex=False)
-	ax      = fig.axes
 	for iax,true_par in enumerate(GLOB_FITS):
 		FITS    = GLOB_FITS[true_par]
-		plotter = SBC_FITS_PLOTTER(iax,ax,[true_par,loop_par,dfpars[loop_par],parlabels[loop_par]],FITS,sbc.bs.choices['analysis_parameters'],sbc.path_to_birdsnack_rootpath)
+		plotter = SBC_FITS_PLOTTER(iax,fig.axes,[true_par,loop_par,dfpars[loop_par],parlabels[loop_par]],FITS,sbc.bs.choices['analysis_parameters'],sbc.path_to_birdsnack_rootpath,quantilemode=args['quantilemode'])
 		plotter.plot_sbc_panel()
 	title_tuples = [l for key,value in non_loop_pars.items() for l in [parlabels[key],value]]
-	ax[0].set_title('Fits to SED-Integrated Simulated Data;\nTrue Simulation Parameters: '+r'$%s = %s ; %s = %s\,$mag'%(title_tuples[0],title_tuples[1],title_tuples[2],title_tuples[3]),fontsize=plotter.FS)
-	ax[-1].set_xlabel(r'$%s$'%parlabels[loop_par],fontsize=plotter.FS)
+	fig.axes[0].set_title('Fits to SED-Integrated Simulated Data;\nTrue Simulation Parameters: '+r'$%s = %s ; %s = %s\,$mag'%(title_tuples[0],title_tuples[1],title_tuples[2],title_tuples[3]),fontsize=plotter.FS)
+	fig.axes[-1].set_xlabel(r'$%s$'%parlabels[loop_par],fontsize=plotter.FS)
 	fig.text(0.01, 0.5, 'Posterior Densities', ha='center', va='center', rotation='vertical',fontsize=plotter.FS)
 	pl.tight_layout()
-	pl.show()
+	if args['save']:
+		pl.savefig(f"{sbc.plotpath}SBC_looppar{loop_par}_nonlooppars{non_loop_pars}_quantilemode{args['quantilemode']}.pdf",bbox_inches='tight')
+	if args['show']:
+		pl.show()
