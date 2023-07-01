fb0bddb25864ebc0a8eeab7d0671f76c38f86b8b
Sam M. Ward <smward1@hotmail.com>
2023-07-01 11:05:51 +0100

    Added additional edit_dicts

--- analysis/analyse_fiducial_sample.py
+++ analysis/analyse_fiducial_sample.py
@@ -9,15 +9,14 @@ dataloader = LOAD_DATA()
 SNSsnpy_fiducial = dataloader.load_SNSsnpy('SNSsnpy_fiducial.pkl')
 dfmeta           = dataloader.dfmeta
 edit_dict        = {}
-#edit_dict = {
-#'preproc_parameters':{'DF_savekey':'uBVriJH','pblist':[s for s in 'uBVriJH']},
-#'analysis_parameters':{'HBM_savekey':'uBVriJH_CensoredCut1.0','lam_choice':'central','CensoredData':True,'CensoredCut':1.0}}
-edit_dict = {'analysis_parameters':{'HBM_savekey':'Fiducial_CensoredCut1.0','CensoredData':True,'CensoredCut':1.0}}
+#edit_dict = {  'preproc_parameters'    :   {'DF_savekey':'uBVriJH','pblist':[s for s in 'uBVriJH']},
+#                'analysis_parameters'   :   {'HBM_savekey':'uBVriJH_CensoredCut1.0','lam_choice':'central','CensoredData':True,'CensoredCut':1.0}}
 #edit_dict = {'analysis_parameters':{'HBM_savekey':'Fiducial_AVGamma_CensoredCut1.0','CensoredData':True,'CensoredCut':1.0,'AVprior':'Gamma','n_sampling':10000}}
+#edit_dict = {'analysis_parameters'      :   {'HBM_savekey':'LowRVs_CensoredCut1.0','CensoredData':True,'CensoredCut':1.0},
+#            'additional_cut_parameters' :   {'extra_drop_SNe':{sn:'Low RVs' for sn in ['2009ds','16abc']}}}
 
 #Load into Bird-Snack
 bs = BIRDSNACK(loader={'SNSsnpy':SNSsnpy_fiducial}, configname='loader_config.yaml', dfmeta=dfmeta, edit_dict=edit_dict)
-
 
 #Get peak magnitudes
 bs.get_peak_mags()
